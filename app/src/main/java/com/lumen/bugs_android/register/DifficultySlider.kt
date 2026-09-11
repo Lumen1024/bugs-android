4127d19cfc8ab94d7fdd6e5e1a9a0d115576ac9a
diserus <prozorenko24@gmail.com>
2026-09-11 22:53:01 +0700

    Changed slider

--- app/src/main/java/com/lumen/bugs_android/register/DifficultySlider.kt
+++ app/src/main/java/com/lumen/bugs_android/register/DifficultySlider.kt
@@ -6,17 +6,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import kotlin.math.roundToInt
 
 @Composable
 fun DifficultySlider() {
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    val levels = listOf("Лёгкая", "Средняя", "Высокая")
+    var index by remember { mutableIntStateOf(0) }
     Column {
+        Text("Уровень сложности")
         Slider(
-            value = sliderPosition,
-            onValueChange = { sliderPosition = it }
+            value = index.toFloat(),
+            onValueChange = { index = it.roundToInt() },
+            valueRange = 0f..2f,
+            steps = 1
         )
-        Text(text = sliderPosition.toString())
+        Text(levels[index])
     }
 }
