d1a8fc2febd4927b76371c8c5a4b3c88761b36c0
lumen1024 <lumenhorizon@icloud.com>
2026-09-11 20:10:03 +0700

    make text center

--- app/src/main/java/com/lumen/bugs_android/MainScreen.kt
+++ app/src/main/java/com/lumen/bugs_android/MainScreen.kt
@@ -1,15 +1,19 @@
 package com.lumen.bugs_android
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
-        Text("Hello World", modifier.padding(innerPadding))
+        Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("Hello World", modifier.padding(innerPadding))
+        }
     }
 }
