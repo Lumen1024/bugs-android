82765e127d89d16e4229d27d25a23d96f47cfe79
diserus <prozorenko24@gmail.com>
2026-09-11 19:41:27 +0700

    Changed version of SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,15 +6,13 @@ plugins {
 android {
     namespace = "com.lumen.bugs_android"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.lumen.bugs_android"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
