f119f9f613f7e0cee5a2fee643cfb399279a0d8c
lumen1024 <lumenhorizon@icloud.com>
2026-09-11 21:37:41 +0700

    move DifficultySlider from RegisterForm.kt

---
+++ app/src/main/java/com/lumen/bugs_android/register/DifficultySlider.kt
@@ -0,0 +1,22 @@
+package com.lumen.bugs_android.register
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun DifficultySlider() {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it }
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
--- app/src/main/java/com/lumen/bugs_android/register/RegisterForm.kt
+++ app/src/main/java/com/lumen/bugs_android/register/RegisterForm.kt
@@ -14,13 +14,11 @@ import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -35,21 +33,13 @@ import com.lumen.bugs_android.getZodiacFromDate
 @Composable
 fun RegisterForm(modifier: Modifier = Modifier) {
     val name = rememberTextFieldState("")
-
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
     var isDialogOpen by remember { mutableStateOf(false) }
     var selectedDateMillis by remember { mutableStateOf<Long?>(null) }
     Column(modifier.fillMaxSize()) {
         OutlinedTextField(state = name,label={ Text("ФИО") })
         GenderMenu()
         CourseSelect()
-        Column {
-            Slider(
-                value = sliderPosition,
-                onValueChange = { sliderPosition = it }
-            )
-            Text(text = sliderPosition.toString())
-        }
+        DifficultySlider()
         Button(onClick = {isDialogOpen= !isDialogOpen}) {
             Text("Выбрать дату")
         }
