8ad40c8b7fe45911df1fe3511cc02bfd454849ed
lumen1024 <lumenhorizon@icloud.com>
2026-09-12 20:40:21 +0700

    add raw navigation

--- app/src/main/java/com/lumen/bugs_android/MainScreen.kt
+++ app/src/main/java/com/lumen/bugs_android/MainScreen.kt
@@ -1,19 +1,117 @@
 package com.lumen.bugs_android
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.lumen.bugs_android.register.RegisterScreenRoot
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data object RegisterScreen
+
+@Serializable
+data object RulesScreen
+
+@Serializable
+data object AuthorsScreen
+
+@Serializable
+data object SettingsScreen
+
+data class NavigationBarRoutes(
+    val route: Any,
+    val label: String,
+    val icon: ImageVector
+)
+
+val navigationBarRoutes = listOf(
+    NavigationBarRoutes(RegisterScreen, "Регистрация", Icons.Default.AccountCircle),
+    NavigationBarRoutes(RulesScreen, "Правила", Icons.Default.Info),
+    NavigationBarRoutes(AuthorsScreen, "Авторы", Icons.Default.Person),
+    NavigationBarRoutes(SettingsScreen, "Настройки", Icons.Default.Settings),
+)
+
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
-        Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            RegisterScreenRoot(modifier.padding(innerPadding))
+    val navController: NavHostController = rememberNavController()
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            val backStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = backStackEntry?.destination
+
+            NavigationBar {
+                navigationBarRoutes.forEach { item ->
+                    val selected = currentDestination?.hierarchy
+                        ?.any { it.hasRoute(item.route::class) } == true
+                    NavigationBarItem(
+                        selected = selected,
+                        onClick = {
+                            navController.navigate(item.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        icon = { Icon(item.icon, contentDescription = item.label) },
+                        label = { Text(item.label) }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = RegisterScreen,
+            modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            composable<RegisterScreen> { RegisterScreenRoot() }
+            composable<RulesScreen> { RulesScreenRoot() }
+            composable<AuthorsScreen> { AuthorsScreenRoot() }
+            composable<SettingsScreen> { SettingsScreenRoot() }
         }
     }
+}
+
+@Composable
+fun SettingsScreenRoot() {
+    Text("Settings")
+}
+
+@Composable
+fun AuthorsScreenRoot() {
+    Text("Authors")
+}
+
+@Composable
+fun RulesScreenRoot() {
+    Text("Rules")
 }
