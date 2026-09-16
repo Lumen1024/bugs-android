217206822b4ac156e3d319fbde165eb11b6ad2ff
lumen1024 <lumenhorizon@icloud.com>
2026-09-16 23:55:41 +0700

    ref: Move NavigationBar to separate file

--- app/src/main/java/com/lumen/bugs_android/MainScreen.kt
+++ app/src/main/java/com/lumen/bugs_android/MainScreen.kt
@@ -2,23 +2,13 @@ package com.lumen.bugs_android
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.lumen.bugs_android.navigation.AppNavigationBar
 import com.lumen.bugs_android.navigation.MainNavHost
-import com.lumen.bugs_android.navigation.navigationBarRoutes
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
@@ -26,35 +16,11 @@ fun MainScreen(modifier: Modifier = Modifier) {
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
-        bottomBar = {
-            val backStackEntry by navController.currentBackStackEntryAsState()
-            val currentDestination = backStackEntry?.destination
-
-            NavigationBar {
-                navigationBarRoutes.forEach { item ->
-                    val selected = currentDestination?.hierarchy
-                        ?.any { it.hasRoute(item.route::class) } == true
-                    NavigationBarItem(
-                        selected = selected,
-                        onClick = {
-                            navController.navigate(item.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        icon = { Icon(item.icon, contentDescription = stringResource(item.labelRes)) },
-                        label = { Text(stringResource(item.labelRes)) }
-                    )
-                }
-            }
-        }
+        bottomBar = { AppNavigationBar(navController = navController) }
     ) { innerPadding ->
         MainNavHost(
             navController = navController,
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
         )
